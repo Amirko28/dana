5614d9a065828a2fb334b10ad96b4d34eb5d4baf
Amir Ketter <amirko28@gmail.com>
2022-11-02 12:42:19 +0200

    add meta tags

--- pages/finish.tsx
+++ pages/finish.tsx
@@ -7,6 +7,11 @@ const FinishPage: NextPage = () => {
         <>
             <Head>
                 <title>דנה כתר</title>
+                <meta property="og:title" content="דנה כתר" />
+                <meta name="description" content="חושבים שמגיע לכם החזר מס?" />
+                <meta property="og:url" content="https://dana-ketter.vercel.app" />
+                <meta property="og:description" content="חושבים שמגיע לכם החזר מס?" />
+                <meta property="og:type" content="website" />
             </Head>
             <Finish />
         </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,11 @@ const Home: NextPage = () => {
         <>
             <Head>
                 <title>דנה כתר</title>
+                <meta property="og:title" content="דנה כתר" />
+                <meta name="description" content="חושבים שמגיע לכם החזר מס?" />
+                <meta property="og:url" content="https://dana-ketter.vercel.app" />
+                <meta property="og:description" content="חושבים שמגיע לכם החזר מס?" />
+                <meta property="og:type" content="website" />
             </Head>
             <LandingPage />
         </>
